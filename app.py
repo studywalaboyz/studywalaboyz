a43b88d9447673f264853476b19e6bf6544cc22e
studywalaboyz <studywalaboyz@gmail.com>
2025-04-15 08:42:41 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,27 +1,48 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+# Dummy user for login example
+users = {'testuser': 'testpass'}
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
+
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        # Authenticate here (database code etc.)
-        return f"Welcome, {username}!"
+        if username in users and users[username] == password:
+            session['username'] = username
+            return redirect(url_for('welcome'))
+        else:
+            return 'Invalid credentials'
     return render_template('login.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        # Handle signup form data
-        return redirect(url_for('login'))
+        username = request.form['username']
+        password = request.form['password']
+        users[username] = password
+        session['username'] = username
+        return redirect(url_for('welcome'))
     return render_template('signup.html')
+
+@app.route('/welcome')
+def welcome():
+    if 'username' in session:
+        return render_template('welcome.html', username=session['username'])
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
 
